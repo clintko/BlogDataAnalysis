22e2ef70aeab54680c9e141bfc59d0fba0163551
clintko <clint00724@gmail.com>
2017-03-08 22:04:10 +0800

    update homework 2

--- Analysis/170302_HW2_RandomVariables.r
+++ Analysis/170302_HW2_RandomVariables.r
@@ -1,24 +1,7 @@
----
-title: "Exercise of Random Variables"
-author: "Kuei Yueh Ko"
-date: "2017/3/2"
-output: html_document
----
 
-1. Random Numbers Generation 
-    - a. Generate 10000 random numbers from Gaussian distribution with mean=20 and variance =10, and plot the distribution. 
-    - b. Generate 10000 random numbers from Binomial distribution with p=0.5 and n=40, and plot the distribution. 
-    - c. Compare the distribution of 1a and 1b, what do you find? 
-    
-2. Make a program that can select our candidates for 
-presentation next week.  This program should select 
-randomly but avoid selecting the numbers that had 
-been selected before.  
-    - (hint: uniform distribution, loop, and if statements.) 
-
-# Prepare the function
-create my functions for each random variable generator
-```{r}
+###########################################################
+# create my functions for each random variable generator
+###########################################################
 # Gaussian Distribution
 myGauss <- function(n, mu, sigma2){
     x <- rnorm(n)
@@ -55,10 +38,10 @@ myBinom <- function(n, size, prob=0.5){
     res <- replicate(n, sum(myBern(size, prob)))
     return(res)
 } # end func myBinom
-```
 
-Helper function to plot matrix
-```{r}
+#########################################
+# Helper function to plot matrix
+#########################################
 plotMatrix <- function(dat, isXlab=T, isYlab=T){
     # given a data (matrix or dataframe), plot the
     # value of each element as color
@@ -86,13 +69,12 @@ plotMatrix <- function(dat, isXlab=T, isYlab=T){
              labels=rev(rownames(dat)), las=2)
     } # end if
 } # end func plotMatrix
-```
 
-
-# 1a Generate 10000 random numbers from Gaussian distribution with mean=20 and variance=10, and plot the distribution
-
-Here I generated standard normal distribution and transform it into a normal distribution equal mean=20, variance equal to 10
-```{r}
+###########################################
+# 1a Generate 10000 random numbers from 
+# Gaussian distribution with mean=20 and 
+# variance=10, and plot the distribution
+###########################################
 # Decide the number of random numbers
 N <- 10000
 
@@ -101,10 +83,7 @@ x <- myGauss(N, 0, 1)
 
 # Shift the distribution into mean=20, variance=10
 y <- ShiftDistr(x, 20, 10)
-```
 
-visualize the distribution of random numbers before and after transformation
-```{r}
 # generate the histogram
 gp1 <- hist(x, plot=FALSE)
 gp2 <- hist(y, plot=FALSE)
@@ -114,31 +93,27 @@ plot(gp1, col=rgb(0,0,1,1/4),
      xlim=c(-10, 40), 
      ylim=c(  0, N/3))
 plot(gp2, col=rgb(1,0,0,1/4), add=T)
-```
 
-
-# 1b. Generate 10000 random numbers from Binomial distribution with p=0.5 and n=40, and plot the distribution. 
-Generate random numbers from binomial distribution, which is the sum of bernoulli random variable (the process of summing bernoulli random variable is implemented in the function myBinom)
-```{r}
+###########################################
+# 1b. Generate 10000 random numbers from 
+# Binomial distribution with p=0.5 and n=40, 
+# and plot the distribution.
+###########################################
 # Decide the number of random numbers
 N <- 10000
 
 # Generate random numbers from binomial distribution
 x <- myBinom(N, 40)
-```
 
-Visualize the distribution of random numbers generated
-```{r}
+# plot the results
 hist(x, col="grey80", xlim=c(0, 40))
 axis(side=1, lwd = 2)
 axis(side=2, lwd = 2)
-```
 
-
-# 1c. Compare the distribution of 1a and 1b, what do you find?
-
-Theoretically, we know that as we increase the number of random number generated, the distribution of binomial distribution would be similar to normal distribution. 
-```{r}
+###########################################
+# 1c. Compare the distribution of 
+#     1a and 1b, what do you find?
+###########################################
 # increase N
 N     <- c(10, 50, 100, 1000)
 Nchar <- as.character(N)
@@ -156,34 +131,33 @@ for (idx in 1:length(N)){
     listGauss[[Nchar[idx]]] <- 
         myGauss(N[idx], mean(x), var(x))
 } # end for loop
-```
 
-Visualize and compare the distribution of Binomial and Gaussian under different N
-```{r}
 # set plot
 par(mfrow=c(2,4))
 
 # plot binomial distribution
 for (idx in Nchar){
     hist(listBinom[[idx]], 
-        col = "#b3cde3",
-        main=paste0("Binomial Distr.\nN=", idx),
-        xlab = "Value")
+         col = "#b3cde3",
+         main=paste0("Binomial Distr.\nN=", idx),
+         xlab = "Value")
 } # end for loop
 
 # plot Gaussian distribution
 for (idx in Nchar){
     hist(listGauss[[idx]], 
-        col = "#fbb4ae",
-        main=paste0("Gauss Distr.\nN=", idx),
-        xlab = "Value")
+         col = "#fbb4ae",
+         main=paste0("Gauss Distr.\nN=", idx),
+         xlab = "Value")
 } # end for loop
-```
 
-# 2. Make a program that can select our candidates for presentation next week. This program should select randomly but avoid selecting the numbers that had been selected before. (hint: uniform distribution, loop, and if statements.) 
-
-create a function to pick two numbers
-```{r}
+##################################################################
+# 2. Make a program that can select our candidates for 
+#    presentation next week. This program should select randomly 
+#    but avoid selecting the numbers that had been selected before. 
+#    (hint: uniform distribution, loop, and if statements.) 
+##################################################################
+# create a function to pick two numbers
 Get2NumRand <- function(ids){
     # pick a student using uniform distribution
     idx <- myUnif(1, 0, length(ids))
@@ -192,7 +166,7 @@ Get2NumRand <- function(ids){
     
     # remove the first chosen student
     ids <- setdiff(ids, id01)
-
+    
     # pick another student from the remain students
     idx <- myUnif(1, 0, length(ids))
     idx <- ceiling(idx)
@@ -201,10 +175,7 @@ Get2NumRand <- function(ids){
     # return result
     return(c(id01, id02))
 } # end func 
-```
 
-Simulate the experiment of choosing two students
-```{r}
 # number of simulation
 N   <- 100000 # 100,000
 
@@ -213,10 +184,7 @@ ids <- 1:18
 
 # simulation
 res <- replicate(N, Get2NumRand(ids))
-```
 
-Convert the result into a matrix by counting each pair of students
-```{r}
 # initiate a matrix to store the result
 mat <- matrix(0, nrow=length(ids), ncol=length(ids))
 
@@ -225,10 +193,8 @@ for (idx in 1:N){
     mat[res[1,idx], res[2,idx]] <- 
         mat[res[1,idx], res[2,idx]] + 1
 } # end for loop
-```
 
-Visualize the result
-```{r}
+
 par(mfrow=c(1,3), 
     #mar=c(5.1,4.1,4.1,2.1))
     mar=c(10,4.1,8,2))
@@ -249,6 +215,3 @@ title(
     main="student selection",
     xlab="Second Student",
     ylab="First Student")
-```
-
-
