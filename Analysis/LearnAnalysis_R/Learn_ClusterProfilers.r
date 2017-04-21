aa0405eb6f312a7041a5e5c1dba13f0d58ddd7e2
clintko <clint00724@gmail.com>
2017-04-21 11:48:42 +0800

    learn r package: clusterprofiliter

---
+++ Analysis/LearnAnalysis_R/Learn_ClusterProfilers.r
@@ -0,0 +1,43 @@
+library(clusterProfiler)
+library(org.Hs.eg.db)
+
+#####################################################
+data(geneList, package="DOSE")
+gene <- names(geneList)[abs(geneList) > 2]
+gene.df <- bitr(gene, fromType = "ENTREZID",
+                toType = c("ENSEMBL", "SYMBOL"),
+                OrgDb = org.Hs.eg.db)
+head(gene.df)
+
+# groupGO is designed for gene classification 
+# based on GO distribution at a specific level
+ggo <- groupGO(gene     = gene,
+               OrgDb    = org.Hs.eg.db,
+               ont      = "CC",
+               level    = 3,
+               readable = TRUE)
+head(ggo)
+
+
+######################################################
+gmtfile <- system.file("extdata", "c5.cc.v5.0.entrez.gmt", package="clusterProfiler")
+c5 <- read.gmt(gmtfile)
+gene
+head(c5)
+egmt <- enricher(gene, TERM2GENE=c5)
+head(egmt)
+
+egmt2 <- GSEA(geneList, TERM2GENE=c5, verbose=FALSE)
+head(egmt2)
+
+
+######################################################
+datAnno <- data.frame(
+    ont=sample(LETTERS[1:5], 100, replace=T),
+    gene=1:100)
+
+lst <- split(datAnno$gene, datAnno$ont)
+datItem <- unique(c(sample(lst$A, 10), sample(1:100, 20)))
+
+res <- enricher(datItem, TERM2GENE=datAnno)
+head(res)
